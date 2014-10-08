a84eb300c9f86c07ff177837dc2cbb7b5c73c404
Simon Lundström <simlu@su.se>
2014-10-08 14:42:03 +0200

    These are fixed.

--- lib/logstash/outputs/soap.rb
+++ lib/logstash/outputs/soap.rb
@@ -7,15 +7,12 @@ class LogStash::Outputs::SOAP < LogStash::Outputs::Base
   config_name 'soap'
   milestone 1
 
-  # FIXME
   # The URI to the WSDL. This can be an URL or a local file. E.g. "https://soap.example.com/?wsdl" or "/var/cache/service.wsdl"
   config :wsdl, :validate => :string, :required => true
 
-  # FIXME
   # The path to the CA file to use for SSL validation, e.g: "/etc/ssl/certs/ca-certificates.crt"
   config :ssl_cafile, :validate => :string, :required => true
 
-  # FIXME
   # The SOAP method we should submit to, e.g: "AddService"
   config :soap_method, :validate => :string, :required => true
 
